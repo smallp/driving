1a243883eb89efd8e02e37513698158ffe85e2a3
Cheng <754672323@qq.com>
2016-08-16 10:15:37 +0800

    change downlist

--- html/scripts/autoComplet.js
+++ html/scripts/autoComplet.js
@@ -23,10 +23,7 @@ $(document).ready(function(){
 		$('#'+target).val($(this).text());
 		$('#'+target+'Id').val($(this).attr('data-id'));
 	});
-	$('input').blur(function(){
-//		$(this).next().css('visibility','hidden');
-	});
-	$('input').focus(function(){
-//		$(this).next().css('visibility','show');
+	$('form').delegate('input','focus',function(){
+		$(this).parent().parent().parent().siblings().find('.completeList').html("");
 	});
 });
